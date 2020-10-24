083e80c2cb0318fce9c564d7f5aaee052c831e6b
Anikait <50035624+anikait1@users.noreply.github.com>
2020-10-24 19:09:37 +0530

    Changed the name of struct used

--- 450Questions/Arrays/kadanesAlgorithm.cpp
+++ 450Questions/Arrays/kadanesAlgorithm.cpp
@@ -2,18 +2,20 @@
 #include <vector>
 #include <algorithm>
 
-struct SubArray {
+// store the information regarding sub arrays
+// start and end index along with max sum
+struct IndexInfo {
   int start;
   int end;
   int sum;
 
-  SubArray(int start, int end, int sum)
+  IndexInfo(int start, int end, int sum)
     : start(start), end(end), sum(sum)
   {
     // constructor
   }
 
-  SubArray()
+  IndexInfo()
   {
     // constructor
   }
@@ -28,31 +30,31 @@ void display(const std::vector<T> &vec) {
   std::cout << "]\n";
 }
 
-SubArray maximumSubArray(const std::vector<int> &vec) {
-  std::vector<SubArray> dp(vec.size());
-  dp[0] = SubArray(0, 0, vec[0]);
+IndexInfo maximumIndexInfo(const std::vector<int> &vec) {
+  std::vector<IndexInfo> dp(vec.size());
+  dp[0] = IndexInfo(0, 0, vec[0]);
 
   for (int i = 1; i < (int)vec.size(); ++i) {
     int sum = std::max(dp[i - 1].sum + vec[i], vec[i]);
 
     if (sum == vec[i]) {
-      dp[i] = SubArray(i, i, vec[i]);
+      dp[i] = IndexInfo(i, i, vec[i]);
     } else {
-      dp[i] = SubArray(dp[i - 1].start, i, dp[i - 1].sum + vec[i]);
+      dp[i] = IndexInfo(dp[i - 1].start, i, dp[i - 1].sum + vec[i]);
     }
   }
 
-  auto maxSubArray = *std::max_element(dp.begin(), dp.end(), [](SubArray &s1, SubArray &s2) {
+  auto maxIndexInfo = *std::max_element(dp.begin(), dp.end(), [](IndexInfo &s1, IndexInfo &s2) {
     return s1.sum < s2.sum;
   });
 
-  return maxSubArray;
+  return maxIndexInfo;
 }
 
 int main() {
   // std::vector<int> vec{-2, 1, -3, 4, -1, 2, 1, -5, 4};
   std::vector<int> vec {1, 2, 3, -2, 5};
-  auto res = maximumSubArray(vec);
+  auto res = maximumIndexInfo(vec);
   std::cout << "Start: " << res.start << " End: " << res.end << " Sum: " << res.sum << '\n';
 
   return 0;
