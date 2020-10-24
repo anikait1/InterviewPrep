29538904971a09251f91a00c053c23dba1e86ad9
Anikait <50035624+anikait1@users.noreply.github.com>
2020-10-25 00:49:03 +0530

    Formatted the code

--- 450Questions/Arrays/kadanesAlgorithm.cpp
+++ 450Questions/Arrays/kadanesAlgorithm.cpp
@@ -4,13 +4,14 @@
 
 // store the information regarding sub arrays
 // start and end index along with max sum
-struct IndexInfo {
+struct IndexInfo
+{
   int start;
   int end;
   int sum;
 
   IndexInfo(int start, int end, int sum)
-    : start(start), end(end), sum(sum)
+      : start(start), end(end), sum(sum)
   {
     // constructor
   }
@@ -22,24 +23,31 @@ struct IndexInfo {
 };
 
 template <typename T>
-void display(const std::vector<T> &vec) {
+void display(const std::vector<T> &vec)
+{
   std::cout << "[ ";
-  for (const auto &val : vec) {
+  for (const auto &val : vec)
+  {
     std::cout << val << ' ';
   }
   std::cout << "]\n";
 }
 
-IndexInfo maximumIndexInfo(const std::vector<int> &vec) {
+IndexInfo maximumIndexInfo(const std::vector<int> &vec)
+{
   std::vector<IndexInfo> dp(vec.size());
   dp[0] = IndexInfo(0, 0, vec[0]);
 
-  for (int i = 1; i < (int)vec.size(); ++i) {
+  for (int i = 1; i < (int)vec.size(); ++i)
+  {
     int sum = std::max(dp[i - 1].sum + vec[i], vec[i]);
 
-    if (sum == vec[i]) {
+    if (sum == vec[i])
+    {
       dp[i] = IndexInfo(i, i, vec[i]);
-    } else {
+    }
+    else
+    {
       dp[i] = IndexInfo(dp[i - 1].start, i, dp[i - 1].sum + vec[i]);
     }
   }
@@ -51,11 +59,14 @@ IndexInfo maximumIndexInfo(const std::vector<int> &vec) {
   return maxIndexInfo;
 }
 
-int main() {
-  // std::vector<int> vec{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-  std::vector<int> vec {1, 2, 3, -2, 5};
+int main()
+{
+  std::vector<int> vec{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  // std::vector<int> vec {1, 2, 3, -2, 5};
   auto res = maximumIndexInfo(vec);
   std::cout << "Start: " << res.start << " End: " << res.end << " Sum: " << res.sum << '\n';
 
   return 0;
 }
+
+// [-2, 1, -3, 4, -1, 2, 1, -5, 4]
