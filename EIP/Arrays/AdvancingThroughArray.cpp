6a0ad57e92c64741d37de0756a716ce0147fb14b
Anikait <50035624+anikait1@users.noreply.github.com>
2020-12-19 18:14:39 +0530

    Added the solution

--- EIP/Arrays/AdvancingThroughArray.cpp
+++ EIP/Arrays/AdvancingThroughArray.cpp
@@ -11,7 +11,18 @@ void display(const std::vector<T>& vec) {
     std::cout << "]\n";
 }
 
-bool CanReachEnd(const std::vector<int>& max_advance_steps)
+bool CanReachEnd(const std::vector<int>& max_advance_steps) {
+    int furthest_reach_end = 0;
+
+    for (int i = 0; i < max_advance_steps.size(); ++i) {
+        if (i > furthest_reach_end || furthest_reach_end >= max_advance_steps.size()) {
+            break;
+        }
+        furthest_reach_end = std::max(furthest_reach_end, i + max_advance_steps[i]);
+    }
+
+    return furthest_reach_end >= max_advance_steps.size(); 
+}
 
 int main() {
 
