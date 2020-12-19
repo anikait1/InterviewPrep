e9ca4b3480224971203736f07f9535cdeec36bf6
Anikait <50035624+anikait1@users.noreply.github.com>
2020-12-19 18:28:28 +0530

    Added the one pass O(n) solution

---
+++ EIP/Arrays/DeleteDuplicates.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void display(const std::vector<T>& vec) {
+    std::cout << "[ ";
+    for (const T& val : vec) {
+        std::cout << val << ' ';
+    }
+    std::cout << "]\n";
+}
+
+// [1, 1, 1, 2, 3, 3, 4, 4, 4, 11, 11, 13]
+// [1, 2, 3, 4, 11, 13]
+int DeleteDuplicates(std::vector<int>* A_ptr) {
+    std::vector<int>& A = *A_ptr;
+
+    if (A.empty()) {
+        return 0;
+    }
+
+    int write_index = 1;
+    for (int i = 1; i < A.size(); ++i) {
+        if (A[i] != A[write_index - 1]) {
+            A[write_index++] = A[i];
+        }
+    }
+    return write_index;
+}
+
+int main() {
+
+    return 0;
+}
