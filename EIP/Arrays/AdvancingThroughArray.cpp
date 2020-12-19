d8198adfe70f7ebfe51092a7145dccb829cef7a7
Anikait <50035624+anikait1@users.noreply.github.com>
2020-12-19 18:11:51 +0530

    Added O(n) solution

---
+++ EIP/Arrays/AdvancingThroughArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void display(const std::vector<T>& vec) {
+    std::cout << "[ ";
+    for (const T& val : vec) {
+        std::cout << val << ' ';
+    }
+    std::cout << "]\n";
+}
+
+bool CanReachEnd(const std::vector<int>& max_advance_steps)
+
+int main() {
+
+    return 0;
+}
