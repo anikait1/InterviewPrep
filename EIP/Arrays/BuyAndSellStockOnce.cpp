fe21e4f0ab220845341a326494233aa1b4d44421
Anikait <50035624+anikait1@users.noreply.github.com>
2020-12-20 17:11:22 +0530

    Added solution for Longest Sub Array With Equal Elements(Variant)

--- EIP/Arrays/BuyAndSellStockOnce.cpp
+++ EIP/Arrays/BuyAndSellStockOnce.cpp
@@ -16,11 +16,24 @@ double BuyAndSellStockOnce(const std::vector<double>& prices) {
     // [100, 120, 110, 140, 200]
     double min_price_so_far = std::numeric_limits<double>::max(), max_profit = 0;
     for (const double& price : prices) {
-        max_profit = std::max(price - min_price_so_far, max_profit);
         min_price_so_far = std::min(min_price_so_far, price);
+        max_profit = std::max(price - min_price_so_far, max_profit);
     }
 
     return max_profit;
+}
+
+int LongestSubArrayWithEqualElements(const std::vector<int>& vec) {
+    int curr_len = 1, max_len = std::numeric_limits<int>::min();
+    for (int i = 1; i < vec.size(); ++i) {
+        if (vec[i] != vec[i - 1]) {
+            max_len = std::max(curr_len, max_len);
+        } else {
+            curr_len++;
+        }
+    }
+
+    return std::max(curr_len, max_len);
 }
 
 int main() {
