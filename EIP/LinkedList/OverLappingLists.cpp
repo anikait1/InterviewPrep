24bbe1646f67313448782728868e7fd634f47d42
Anikait <anikait.makkar@outlook.com>
2020-12-25 11:42:12 +0530

    Added O(n) time and O(n) space solution

---
+++ EIP/LinkedList/OverLappingLists.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <unordered_set>
+
+template<typename T>
+struct ListNode {
+    T data;
+    ListNode* next;
+
+    ListNode(T data)
+        : data(data), next(nullptr)
+    {
+        // constructor
+    }
+};
+
+template <typename T>
+void display(ListNode<T>* node) {
+    std::cout << "[ ";
+    while (node) {
+        std::cout << node->data << ' ';
+        node = node->next;
+    }
+    std::cout << "]\n";
+}
+
+ListNode<int>* OverlappingNodes(ListNode<int>* L1, ListNode<int>* L2) {
+    std::unordered_set<ListNode<int>*> nodes;
+
+    while (L1) {
+        nodes.insert(L1);
+        L1 = L1->next;
+    }
+
+    while (L2) {
+        if (nodes.find(L2) != nodes.end()) {
+            return L2;
+        } else {
+            nodes.insert(L2);
+        }
+
+        L2 = L2->next;
+    }
+
+    return nullptr;
+}
+
+int main() {
+
+    return 0;
+}
