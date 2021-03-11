8332dbb6c2264efa64ea668769eb91a7008965a8
Anikait <anikait.makkar@outlook.com>
2021-03-11 16:03:19 +0530

    Updated changes

---
+++ EIP/BinaryTrees/HasPathSum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+struct BinaryTreeNode {
+    int data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+
+    BinaryTreeNode(int x)
+        : data(x), left(nullptr), right(nullptr)
+        {
+
+        }
+};
+
+
+bool HasPath(BinaryTreeNode* node, int sum) {
+}
+
+
+int main() {
+
+    return 0;
+}
